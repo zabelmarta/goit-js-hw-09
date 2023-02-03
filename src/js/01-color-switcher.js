b78c5b24aca733beb5cc9c02912ab8ea60e88c08
zabelmarta <104087600+zabelmarta@users.noreply.github.com>
2023-02-03 21:24:57 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let makeChange = 0;
+
+
+start.addEventListener('click', () => {
+    
+    makeChange = setInterval(() => {    
+    body.style.backgroundColor = getRandomHexColor();           
+    start.setAttribute("disabled", true);
+
+    }, 1000);
+    
+});
+
+stop.addEventListener('click', () => {
+    
+    clearInterval(makeChange);    
+    start.removeAttribute("disabled");
+    
+});
